e32a843fbeee4ddaaba3a54f908f26855389503e
a1243017991 <846107788@qq.com>
2023-02-14 20:15:34 +0800

    频繁请求返回值修改，RateLimit预留Redis注入

--- ChatGPTApi/AppStart/RateLimitRegister.cs
+++ ChatGPTApi/AppStart/RateLimitRegister.cs
@@ -11,9 +11,24 @@ namespace ChatGPTApi.AppStart
             {
                 config.AddJsonFile("ratelimit.json", optional: true, reloadOnChange: true);//拦截单独的配置
             });
+
+            //注入日配置
             services.Configure<IpRateLimitOptions>(builder.Configuration.GetSection("IpRateLimiting"));
 
+            //注入计数器和规则存储
+
+            //内存
+            services.AddSingleton<IIpPolicyStore, MemoryCacheIpPolicyStore>();
+            services.AddSingleton<IClientPolicyStore, MemoryCacheClientPolicyStore>();
+            services.AddSingleton<IRateLimitCounterStore, MemoryCacheRateLimitCounterStore>();//计数器
             services.AddInMemoryRateLimiting();
+
+            //redis
+            //services.AddSingleton<IRateLimitCounterStore, DistributedCacheRateLimitCounterStore>();
+            //services.AddSingleton<IIpPolicyStore, DistributedCacheIpPolicyStore>();
+            //services.AddSingleton<IClientPolicyStore, DistributedCacheClientPolicyStore>();
+            //services.AddRedisRateLimiting();
+
             services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
         }
     }
